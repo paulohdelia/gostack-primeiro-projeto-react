ad23b6fcea7e2239e146d1e4599a765b9c5f69af
paulohdelia <paulohdelia@gmail.com>
2020-12-04 12:38:34 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
