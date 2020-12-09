379bc2a4c4fecf6ef0665aec6d2af9434b23bf95
paulohdelia <paulohdelia@gmail.com>
2020-12-09 15:50:00 -0300

    add dashboard style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/github-explorer-logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import { Title, Form, Repositories } from './styles';
 
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars0.githubusercontent.com/u/47276018?s=60&v=4"
+          alt="Paulo D'Elia"
+        />
+
+        <div>
+          <strong>paulohdelia/gobarber-gostack</strong>
+          <p>Desenvolvendo o projeto durante o bootcamp GoStack</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 export default Dashboard;
